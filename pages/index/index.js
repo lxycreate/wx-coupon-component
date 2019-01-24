70cbf63c2b45dd67feed932f7061e102c21dfc67
李先营 <298151501@qq.com>
2019-01-24 14:03:02 +0800

    调整搜索框样式,调整分类样式

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,66 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    cid_btns: [{
+      name: '全部',
+      value: '0',
+      class_name: 'all',
+      icon_class_name: 'icon-yingyong'
+    }, {
+      name: '女装',
+      value: '1',
+      class_name: 'dress',
+      icon_class_name: 'icon-qunzi'
+    }, {
+      name: '男装',
+      value: '2',
+      class_name: 'man',
+      icon_class_name: 'icon-xizhuang'
+    }, {
+      name: '内衣',
+      value: '3',
+      class_name: 'underwear',
+      icon_class_name: 'icon-neiyi'
+    }, {
+      name: '数码',
+      value: '4',
+      class_name: 'digital',
+      icon_class_name: 'icon-bijiben'
+    }, {
+      name: '美食',
+      value: '5',
+      class_name: 'food',
+      icon_class_name: 'icon-shiwu'
+    }, {
+      name: '美妆',
+      value: '6',
+      class_name: 'beauty',
+      icon_class_name: 'icon-hufupin'
+    }, {
+      name: '母婴',
+      value: '7',
+      class_name: 'infant',
+      icon_class_name: 'icon-naiping'
+    }, {
+      name: '鞋包',
+      value: '8',
+      class_name: 'shoes',
+      icon_class_name: 'icon-nvbao'
+    }, {
+      name: '家居',
+      value: '9',
+      class_name: 'household',
+      icon_class_name: 'icon-chuang'
+    }, {
+      name: '文体',
+      value: '10',
+      class_name: 'sport',
+      icon_class_name: 'icon-bangqiu'
+    }, {
+      name: '其他',
+      value: '11',
+      class_name: 'else',
+      icon_class_name: 'icon-qita'
+    }]
   }
 })
